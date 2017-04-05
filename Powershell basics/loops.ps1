491925bcbde34afd3d4cc5f73a2c2efebc472ac3
Łukasz Księżak <lukasz.ksiezak@interia.pl>
2017-04-05 22:19:24 +0200

    cleanup of temp content

--- Powershell basics/loops.ps1
+++ Powershell basics/loops.ps1
@@ -2,30 +2,3 @@ $myDocuments = Get-ChildItem $env:USERPROFILEDocuments -File
 $myDocuments | ForEach-Object {$_.FullName}
 ForEach-Object -InputObject $myDocuments -Process {$_.FullName}
 
-For ($i=0; $i -le 10; $i++) {
-    "10 * $i = " + (10 * $i)
-    }
-
-$i=1
-Do {
-    $i
-    $i++
-    }
-While ($i -le 10)
-
-$i=1
-Do {
-    $i
-    $i++
-    }
-Until ($i -gt 10)
-
-$i=1
-While ($true)
-    {
-    $i
-    $i++
-    if ($i -gt 10) {
-        Break
-        }
-    }
